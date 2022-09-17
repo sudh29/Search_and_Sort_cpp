884ea865cc99f8cb7cb4f87f3d76429c2c48c756
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-09-17 17:50:11 +0530

    Update 14_Zero_Sum_Subarrays.cpp

--- 14_Zero_Sum_Subarrays.cpp
+++ 14_Zero_Sum_Subarrays.cpp
@@ -4,20 +4,17 @@ class Solution{
     ll findSubarray(vector<ll> arr, int n ) {
         ll res=0;
         ll curr_sum=0;
-        unordered_map<ll,ll> sum_map;
+        unordered_map<ll,vector<int>> sum_map;
     
         for(int i=0;i<n;i++){
             curr_sum+=arr[i];
-            if (sum_map.find(curr_sum) != sum_map.end()){
-                res+=sum_map[curr_sum];
-                sum_map[curr_sum]++;
-            }
-            else{
-                sum_map.insert({curr_sum,1});
-            }
             if(curr_sum==0){
                 res++;
             }
+            if (sum_map[curr_sum].size() != 0){
+                res+=sum_map[curr_sum].size();
+            }
+            sum_map[curr_sum].push_back(i);
         }
         return res;
     }
